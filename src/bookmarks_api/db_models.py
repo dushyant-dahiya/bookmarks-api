a513cafad67b8f771fbb98cb25e211b02c2c289f
Dushyant Dahiya <dushyant.dahiya88@gmail.com>
2026-09-16 19:25:34 -0700

    Add SQLAlchemy database connection and Bookmark ORM model

---
+++ src/bookmarks_api/db_models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import ARRAY, Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class BookmarkORM(Base):
+    __tablename__ = "bookmarks"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    url: Mapped[str] = mapped_column(String, nullable=False)
+    title: Mapped[str] = mapped_column(String, nullable=False)
+    tags: Mapped[list[str]] = mapped_column(ARRAY(String), default=[])
+    notes: Mapped[str] = mapped_column(String, default="")
--- src/bookmarks_api/models.py
+++ src/bookmarks_api/models.py
@@ -10,25 +10,3 @@ class BookmarkCreate(BaseModel):
 
 class Bookmark(BookmarkCreate):
     id: int
-
-
-database_db: dict[int, Bookmark] = {}
-next_id = 1
-
-
-def create_bookmark(bookmark: BookmarkCreate) -> Bookmark:
-    global next_id
-    new_bookmark = Bookmark(
-        id=next_id, url=bookmark.url, title=bookmark.title, tags=bookmark.tags, notes=bookmark.notes
-    )
-    database_db[next_id] = new_bookmark
-    next_id += 1
-
-    return new_bookmark
-
-
-def list_bookmarks() -> list[Bookmark]:
-    bookmarks = []
-    for bookmark in database_db.values():
-        bookmarks.append(bookmark)
-    return bookmarks
